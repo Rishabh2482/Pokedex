ea4c73db7c9eb71d3bb6cbb04c74e12cfad3162e
Rishabh2482 <rishabh2002chauhan@gmail.com>
2025-01-09 01:17:28 +0530

    Added Pageination (Multiple Pages rendered for next 20 pokemons)

--- src/components/PokemonList/PokemonList.jsx
+++ src/components/PokemonList/PokemonList.jsx
@@ -7,11 +7,22 @@ function PokemonList(){
     const[pokemonList, setPokemonList]= useState([]);
     const [isLoading, setIsLoading]= useState(true);
 
-    const POKEDEX_URL = 'https://pokeapi.co/api/v2/pokemon/'
+    const [POKEDEX_URL, setPOKEDEX_URL] = useState('https://pokeapi.co/api/v2/pokemon/')
+
+    const [nextUrl, setNextUrl] = useState('');
+    const [prevUrl, setPrevUrl] = useState('');
+
     async function downloadPokemons() {
+        setIsLoading(true);
+
         const response = await axios.get(POKEDEX_URL);      // This downloads list of 20 pokemons
 
         const pokemonResults= response.data.results;        // We get array of pokemons from results
+
+        console.log(response.data);
+        setNextUrl(response.data.next);
+        setPrevUrl(response.data.previous);
+
         const pokemonResultPromise = pokemonResults.map((pokemon) => axios.get(pokemon.url));   // This fetches the data of each pokemon 
 
 
@@ -38,7 +49,7 @@ function PokemonList(){
 
     useEffect(()=>{
         downloadPokemons();
-    },[]);
+    },[POKEDEX_URL]);       // As soon as POKEDEX_URL is updated downloadPokemons function will be rerendered.
 
     return ( 
         <>
@@ -52,8 +63,8 @@ function PokemonList(){
                     }
                 </div>
                 <div className="controls">
-                    <button>Prev</button>
-                    <button>Next</button>
+                    <button disabled={prevUrl==null} onClick={()=> setPOKEDEX_URL(prevUrl)}>Prev</button>
+                    <button disabled={nextUrl==null} onClick={()=> setPOKEDEX_URL(nextUrl)}>Next</button>
                 </div>
             </div>
         </>
